619b4a7173dd57bf5465d169fc0e1cbd4c3cb612
rmr6894 <rmr6894@rit.edu>
2017-11-20 19:17:49 -0500

    init

---
+++ JellyfishBot/Modules/JellyfishBotCommands.cs
@@ -0,0 +1,32 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JellyfishBot.Modules
+{
+    public class JellyfishBotCommands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task PingAsync()
+        {
+            EmbedBuilder builder = new EmbedBuilder();
+            Color myRgbColor = new Color(255, 102, 179);
+
+            builder.AddField("Ping!", "*bloop* ~ Pong!")
+                .WithColor(myRgbColor);
+
+            await ReplyAsync("", false, builder.Build());
+        }
+
+        [Command("echo")]
+        public async Task EchoAsync([Remainder] string stuffToEcho)
+        {
+            stuffToEcho = "*" + stuffToEcho + "*";
+
+            await ReplyAsync(stuffToEcho + "\n" + stuffToEcho + "\n" + stuffToEcho);
+        }
+    }
+}
