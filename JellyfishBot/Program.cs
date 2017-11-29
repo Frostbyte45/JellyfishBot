c878205bd08f59d839351def6c6916b405e2113e
Nia Specht <calvcat4@gmail.com>
2017-11-28 23:55:49 -0500

    Arbitrary changes

--- JellyfishBot/Program.cs
+++ JellyfishBot/Program.cs
@@ -70,7 +70,7 @@ namespace JellyfishBot
 
                 if (!result.IsSuccess)
                     Console.WriteLine(result.ErrorReason);
-                await Task.Delay(2000);
+                await Task.Delay(1500);
                 await arg.DeleteAsync();
             }
 
